05fc66a73e9546563c7e0cdef3b8624878e0fc44
Aakansha36 <101129131+Aakansha36@users.noreply.github.com>
2024-04-25 14:14:41 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,22 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+    def__init__(self):
+        self.stack = []
+        self.mapping = { ')' : '(' , '}': '{', ']':'['}
+        def isValid(self, s):
+            " " "
+            :type s: str
+            :rtype:bool
+            " " "
+            self.stack = []
+            for char in s:
+                if char in self.mapping:
+                top_element = self.stack.pop()
+                 if self.stack  else '#' if self.mapping[char] != top_element:
+                    return False
+                    else:
+                        self.stack.append(char)
+                        return not self.stack
+   
     
 
 
